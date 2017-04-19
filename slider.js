43c981c598e99f1be7093af0bb585dec1b586cb8
Taylor Ann Stillman <stilltay@gmail.com>
2017-04-19 15:25:59 -0400

    Fixed slider

--- slider.js
+++ slider.js
@@ -1,6 +1,25 @@
-$(document).ready(function ( ) {
-    $('.slider').slick({
-        adaptiveHeight: true
-  });
-});
+var slideIndex = 0;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  console.log(slides.length);
+  console.log("Slide index before if: %i", slideIndex);
+  if (n >= slides.length) {slideIndex = 0}
+  console.log("Slide index after if: %i", slideIndex);
+  if (n < 0) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  console.log(slideIndex);
+  slides[slideIndex].style.display = "block";
+}
