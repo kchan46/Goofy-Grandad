06cc0621e41a1ea572c99844863485f215d21ca4
gxu36 <gxu18@gatech.edu>
2017-09-08 09:12:35 -0400

    changed js code to make the slider fully function.

--- slider.js
+++ slider.js
@@ -1,25 +1,17 @@
 var slideIndex = 0;
-showSlides(slideIndex);
+showSlides();
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    slideIndex += n;
+    showSlides();
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  console.log(slides.length);
-  console.log("Slide index before if: %i", slideIndex);
-  if (n >= slides.length) {slideIndex = 0}
-  console.log("Slide index after if: %i", slideIndex);
-  if (n < 0) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    slideIndex = (slideIndex + slides.length) % slides.length;
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-  }
-  console.log(slideIndex);
-  slides[slideIndex].style.display = "block";
+    }
+    slides[slideIndex].style.display = "block";
 }
