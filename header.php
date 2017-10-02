1346c2e6ae35b3f20ab82cf8ecc17309a415db49
Taylor Ann Stillman <stilltay@gmail.com>
2017-10-02 12:24:46 -0400

    added error messages for incomplete pages

--- header.php
+++ header.php
@@ -11,12 +11,20 @@
                 About</a></div>
         <div class="nav-item"><a href="https://www.youtube.com/channel/UCqppi0-9FmF-o3b4gDNUWng/featured"
                                  target="_blank">Videos</a></div>
-        <div class="nav-item"><a href="shop.html">Shop</a></div>
-        <div class="nav-item"><a href="events.html">Events</a></div>
-        <div class="nav-item"><a href="review.html">Review</a></div>
+         <div class="nav-item" >
+            <a href="shop.php" <?php if ($currentPage == "shop") {echo "id=\"current_nav_item\"";}?>>
+                Shop</a></div>
+         <div class="nav-item" >
+            <a href="events.php" <?php if ($currentPage == "events") {echo "id=\"current_nav_item\"";}?>>
+                Events</a></div>
+         <div class="nav-item" >
+            <a href="review.php" <?php if ($currentPage == "review") {echo "id=\"current_nav_item\"";}?>>
+                Review</a></div>
         <div class="nav-item" >
             <a href="contact.php" <?php if ($currentPage == "contact") {echo "id=\"current_nav_item\"";}?>>
                 Contact</a></div>
-        <div class="nav-item"><a href="faq.html">FAQ</a></div>
+         <div class="nav-item" >
+            <a href="faq.php" <?php if ($currentPage == "faq") {echo "id=\"current_nav_item\"";}?>>
+                FAQ</a></div>
     </div>
 </div>
