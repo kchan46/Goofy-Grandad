c1e701faad8ce7d80437e8c74f518e376822e841
gxu36 <benxgh1996@gmail.com>
2017-10-15 10:33:11 -0400

    Deleted shop tab from the nav bar.

--- header.php
+++ header.php
@@ -10,20 +10,17 @@
             <a href="about.php" <?php if ($currentPage == "about") {echo "id=\"current_nav_item\"";}?>>
                 About</a></div>
         <div class="nav-item"><a href="https://www.youtube.com/channel/UCqppi0-9FmF-o3b4gDNUWng/featured"
-                                 target="_blank">Videos</a></div>
-         <div class="nav-item" >
-            <a href="shop.php" <?php if ($currentPage == "shop") {echo "id=\"current_nav_item\"";}?>>
-                Shop</a></div>
-         <div class="nav-item" >
+                             target="_blank">Videos</a></div>
+        <div class="nav-item" >
             <a href="events.php" <?php if ($currentPage == "events") {echo "id=\"current_nav_item\"";}?>>
                 Events</a></div>
-         <div class="nav-item" >
-            <a href="review.php" <?php if ($currentPage == "review") {echo "id=\"current_nav_item\"";}?>>
-                Review</a></div>
+        <div class="nav-item" >
+        <a href="review.php" <?php if ($currentPage == "review") {echo "id=\"current_nav_item\"";}?>>
+            Review</a></div>
         <div class="nav-item" >
             <a href="contact.php" <?php if ($currentPage == "contact") {echo "id=\"current_nav_item\"";}?>>
                 Contact</a></div>
-         <div class="nav-item" >
+        <div class="nav-item" >
             <a href="faq.php" <?php if ($currentPage == "faq") {echo "id=\"current_nav_item\"";}?>>
                 FAQ</a></div>
     </div>
